bf215906434c671b93db4ee67922d4c816d2c5f4
Kevin T. Leroux <kev.tl63@gmail.com>
2024-08-21 17:50:40 +0200

    chercher ville + icones base temps

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
   const apiKey = '47f1c97685c2671f61983cdb9ef458a6'
-  const city = 'Thiers' // Change this to the desired city
+  const cityInput = document.querySelector('.city-input')
+  const getWeatherButton = document.querySelector('.get-weather-btn')
+  const errorMessageElement = document.querySelector('.error-message')
+
+  getWeatherButton.addEventListener('click', () => {
+    const city = cityInput.value
+    getWeatherData(city)
+  })
 
   const cityElement = document.querySelector('.city')
   const temperatureElement = document.querySelector('.temperature')
   const conditionElement = document.querySelector('.condition')
 
-  async function getWeatherData() {
+  async function getWeatherData(city = 'Paris') {
     try {
       const response = await fetch(
-        `http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`
+        `https://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`
       )
       const data = await response.json()
 
@@ -20,11 +27,26 @@ document.addEventListener('DOMContentLoaded', function () {
       cityElement.textContent = data.location.name
       temperatureElement.textContent = `${data.current.temperature}°C`
       conditionElement.textContent = data.current.weather_descriptions[0]
+      updateBackground(data.current.weather_descriptions[0])
+      updateWeatherIcon(data.current.weather_descriptions[0])
+      errorMessageElement.style.display = 'none'
     } catch (error) {
       console.error('Error fetching weather data:', error)
       cityElement.textContent = 'Error'
+      errorMessageElement.style.display = 'block'
     }
   }
 
   getWeatherData()
+
+  function updateBackground(condition) {
+    document.body.className = condition.toLowerCase().replace(/\s+/g, '-')
+  }
+
+  function updateWeatherIcon(condition) {
+    const iconElement = document.querySelector('.weather-icon')
+    const iconName = condition.toLowerCase().replace(/\s+/g, '-')
+    iconElement.src = `icons/${iconName}.png`
+    iconElement.alt = condition
+  }
 })
